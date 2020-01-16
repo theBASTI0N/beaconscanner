f43b4888ee6c83e93a1e1c7695d2ada2815646ce
BASTION <anthony.smigielski@gmail.com>
2020-01-16 10:20:07 +0800

    v0.8 - removed unused code

--- beaconscanner/scanner.py
+++ beaconscanner/scanner.py
@@ -5,7 +5,7 @@ from importlib import import_module
 from binascii import hexlify
 from beacondecoder import decode
 from .device_filters import DeviceFilter
-from .utils import is_packet_type, to_int, bin_to_int, get_mode, bt_addr_to_string
+from .utils import is_packet_type, to_int, bin_to_int, get_mode
 from .const import (ScanType, ScanFilter, BluetoothAddressType,
                     LE_META_EVENT, OGF_LE_CTL, OCF_LE_SET_SCAN_ENABLE,
                     OCF_LE_SET_SCAN_PARAMETERS, EVT_LE_ADVERTISING_REPORT,
